d5243d94c13ffb2dcebc5550166be52c4723b058
prathaprenu <prathaprenu143@gmail.com>
2023-11-02 12:12:53 +0530

    first commit

---
+++ src/test/java/com/example/prathap/PrathapApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.prathap;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PrathapApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
